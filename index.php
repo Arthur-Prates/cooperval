bee75d359c717b41a08aa1469e76ad4ba95a43c9
Arthur-Prates <arthurprateslima@gmail.com>
2024-05-03 16:43:06 -0300

    pastas inicial

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include_once("config/constantes.php");
+include_once("config/conexao.php");
+include_once("func/funcoes.php");
+
+?>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Title</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="./css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css">
+</head>
+
+<body>
+
+<div class="container-fluid mt-5">
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/0.9.0/jquery.mask.min.js" integrity="sha512-oJCa6FS2+zO3EitUSj+xeiEN9UTr+AjqlBZO58OPadb2RfqwxHpjTU8ckIC8F4nKvom7iru2s8Jwdo+Z8zm0Vg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="./js/controle.js"></script>
+
+</body>
+
+</html>
+
---
+++ js/controle.js
@@ -0,0 +1,28 @@
+function carregarConteudo(controle){
+    fetch('controle.php', {
+        method: 'POST', headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }, body: 'controle=' + encodeURIComponent(controle),
+    })
+        .then(response => response.text())
+        .then(data => {
+
+            document.getElementById('show').innerHTML= data;
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+        });
+}
+
+// $('.cpf').mask('000.000.000-00');
+//
+// var options = {
+//     onKeyPress: function (tell, e, field, options) {
+//         var masks = ['(00) 0 0000-0000', '(00) 0000-0000'];
+//         var mask = (tell.length < 15) ? masks[1] : masks[0];
+//         $('.telefoneBR').mask(mask, options);
+//     }
+// };
+// $('.telefoneBR').mask('(00) 0 0000-0000', options);
+//
+//
